a58d90d7b7c13b93404936f566630823de07f160
Chaitanya <puranikc@shernet.sheridancollege.ca>
2024-06-09 15:25:22 -0400

    Add project files.

---
+++ DriveWellApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DriveWellApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DriveWellApp/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace DriveWellApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+
+}
