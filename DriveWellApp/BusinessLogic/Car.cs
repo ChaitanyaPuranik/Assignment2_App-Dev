02b754d866f111834cbf81276bc06f5d313cf415
Chaitanya <puranikc@shernet.sheridancollege.ca>
2024-06-09 16:41:41 -0400

    Created class Car and enum CarType in BusinessLogic layer.

---
+++ DriveWellApp/BusinessLogic/Car.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveWellApp.BusinessLogic
+{
+    class Car
+    {
+        string _vin;
+        string _carMake;
+        CarType _carType;
+        float _price;
+        int _modelYear;
+
+        public Car(string vin, string carMake, CarType carType, float price, int modelYear)
+        {
+            VIN = vin;
+            CarMake = carMake;
+            CarType = carType;
+            Price = price;
+            ModelYear = modelYear;
+        }
+
+        #region Properties
+        public string VIN 
+        {
+            get { return _vin; }
+            private set 
+            { 
+                if( value.Length <= 0 || value.Length > 17)
+                {
+                    throw new Exception("VIN number must be of 17 alphanumeric characters");
+                }            
+                _vin = value;
+            }
+        }
+
+        public string CarMake
+        {
+            get { return _carMake; }
+            private set 
+            {
+                if(value.Length == 0)
+                {
+                    throw new Exception("Car Make cannot be empty");
+                }            
+                _carMake = value;
+            }
+        }
+
+        public CarType CarType
+        {
+            get { return _carType; }
+            private set { _carType = value; }
+        }
+
+        public float Price
+        {
+            get { return _price; }
+            private set 
+            {
+                if(value <= 0)
+                {
+                    throw new Exception("Invalid Price.");
+                }
+                _price = value;
+            }
+        }
+
+        public int ModelYear
+        {
+            get { return _modelYear; }
+            private set { value = _modelYear; }
+        }
+        #endregion
+    }
+}
---
+++ DriveWellApp/BusinessLogic/CarType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveWellApp.BusinessLogic
+{
+    enum CarType
+    {
+        SUV,
+        Coupe,
+        Hatchback,
+        Sedan
+    }
+    
+}
